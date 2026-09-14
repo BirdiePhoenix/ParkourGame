08c168527c9388d7979c7cf9fb9d55e1382938c1
Svenebanan1 <Casper.aaroe@hotmail.com>
2026-09-14 15:38:50 +0200

    SPikes, Moving platforms

---
+++ Assets/Casper Scripts/PlatFormMovingUpDown.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlatformMovementUpDown : MonoBehaviour
+{
+    private Vector3 _startPosition;
+
+    [SerializeField] private float _elapsedTime;
+    [SerializeField] private float _frequency = 2f;
+    [SerializeField] private float _amplitude = 3f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _startPosition = transform.position;
+
+        _elapsedTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _elapsedTime += Time.deltaTime * _frequency;
+        transform.position = _startPosition + Vector3.up * Mathf.Sin(_elapsedTime) * _amplitude;
+    }
+}
