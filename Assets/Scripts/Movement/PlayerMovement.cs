9d676e1c3538077aa6f0cb8f3f5015851547231f
Felix Johansson <felix.william1993@gmail.com>
2026-09-14 13:36:14 +0200

    Started to convert to firstperson

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -4,16 +4,39 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
-    private Vector2 moveInput;
+    //private Vector2 moveInput;
     [SerializeField] private float jumpForce = 100f;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Camera mainCamera;
+    private CharacterController characterController;
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private float currentMoveSpeed = 0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private float lookAngle = 0f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Start()
+    {
+        mainCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();
+
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+
+        currentMoveSpeed = moveSpeed;
+    }
+
     private void Update()
     {
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
         rb.linearVelocity = new Vector3(moveInput.x, rb.linearVelocity.y, moveInput.y);
     }
 
@@ -29,6 +52,6 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnMovement(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        //moveInput = context.ReadValue<Vector2>();
     }
 }
