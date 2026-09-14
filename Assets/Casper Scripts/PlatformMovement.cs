1ca24bed6241ac4a16447c3d1fcdb91016332e7c
Svenebanan1 <Casper.aaroe@hotmail.com>
2026-09-14 13:57:09 +0200

    Timer

--- Assets/Casper Scripts/PlatformMovement.cs
+++ Assets/Casper Scripts/PlatformMovement.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
+    private Vector3 _startPosition;
+
+    private float _elapsedTime;
+    private float _frequency = 2f;
+    private float _amplitude = 3f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _startPosition = transform.position;
+
+        _elapsedTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _elapsedTime += Time.deltaTime * _frequency;
+        transform.position = _startPosition + Vector3.up * Mathf.Sin(_elapsedTime) * _amplitude;
     }
 }
