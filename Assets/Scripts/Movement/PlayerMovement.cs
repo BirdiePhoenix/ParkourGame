dfc3fd5724a0393643e7d919faaa777d03b037bd
Felix Johansson <felix.william1993@gmail.com>
2026-09-15 11:56:10 +0200

    Fixed Movement
    Movement + Look + Jump + Crouch

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
+    private InputSystem_Actions inputActions;
     //private Vector2 moveInput;
     [SerializeField] private float jumpForce = 100f;
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float mouseSensitivity = 0.1f;
+    [SerializeField] private float gamePadSensitivity = 0.1f;
+    private float cameraPitch = 0f;
+    private bool isSliding = false;
+
 
     private Camera mainCamera;
-    private CharacterController characterController;
-
+    
     private InputAction moveAction;
     private InputAction jumpAction;
+    private InputAction sprintAction;
+    private InputAction lookAction;
+    private InputAction slideAction;
 
     private float currentMoveSpeed = 0f;
     private Vector3 moveDirection = Vector3.zero;
@@ -21,15 +32,62 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        lookAction = InputSystem.actions.FindAction("Look");
+        slideAction = InputSystem.actions.FindAction("Crouch");
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnEnable()
+    {
+        jumpAction.performed += JumpAction_performed;
+        slideAction.performed += SlideAction_performed;
+        slideAction.canceled += SlideAction_canceled;
+    }
+
+    private void OnDisable()
+    {
+        jumpAction.performed -= JumpAction_performed;
+        slideAction.performed -= SlideAction_performed;
+    }
+
+    private void JumpAction_performed(InputAction.CallbackContext obj)
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0));
+        }
+    }
+
+    private void SlideAction_performed(InputAction.CallbackContext obj)
+    {
+        if(!isGrounded)
+            return;
+        Debug.Log(obj); 
+
+        transform.localScale *= 0.5f;
+        isSliding = true;
+    }
+
+    private void SlideAction_canceled(InputAction.CallbackContext obj)
+    {
+        if (isSliding)
+        {
+            transform.localScale *= 2f;
+        }
+        isSliding = false;
+
     }
 
     private void Start()
     {
         mainCamera = GetComponentInChildren<Camera>();
-        characterController = GetComponent<CharacterController>();
+        //characterController = GetComponent<CharacterController>();
 
-        moveAction = InputSystem.actions.FindAction("Move");
-        jumpAction = InputSystem.actions.FindAction("Jump");
+        
 
         currentMoveSpeed = moveSpeed;
     }
@@ -37,17 +95,18 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        rb.linearVelocity = new Vector3(moveInput.x, rb.linearVelocity.y, moveInput.y);
-    }
+        Vector2 lookInput = lookAction.ReadValue<Vector2>();
+        //float currentMouseSensitivity = inputActions.controlSchemes == "Gamepad" ? gamePadSensitivity : mouseSensitivity;
+        float mouseX = lookInput.x;
+        float mouseY = lookInput.y;
+        rb.MovePosition(rb.position + ((transform.forward * moveInput.y) + (transform.right * moveInput.x)) * moveSpeed * Time.deltaTime);
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, LayerMask.GetMask("Ground"));
 
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            Debug.Log(context);
-            rb.AddForce(new Vector3(0, jumpForce, 0));
-        }
-        
+        cameraPitch -= mouseY * mouseSensitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX * mouseSensitivity);
+        //rb.linearVelocity = new Vector3(moveInput.x, rb.linearVelocity.y, moveInput.y);
     }
 
     public void OnMovement(InputAction.CallbackContext context)
