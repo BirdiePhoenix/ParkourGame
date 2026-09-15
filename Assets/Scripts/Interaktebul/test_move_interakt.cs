cdabd70ef4d553d57167c7a84a530f2ada542f58
ludwig2026 <ludwiggustafsson129@gmail.com>
2026-09-15 12:42:23 +0200

    sphere

---
+++ Assets/Scripts/Interaktebul/test_move_interakt.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class test_move_interakt_start : MonoBehaviour
+{
+    public float PickUpCrystalStart = 0;
+    public test_move_interakt_start_end test_move_interakt_start_end;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (test_move_interakt_start_end.PickUpCrystalEnd == 1)
+        {
+            PickUpCrystalStart = 0;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (test_move_interakt_start_end.PickUpCrystalEnd == 0 && PickUpCrystalStart == 0)
+        {
+            Debug.Log("time start");
+            PickUpCrystalStart = 1;
+        } 
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("exit");
+    }
+}
