77bd2af299ff34f01f8f74b6e6e861bf2a32ca0a
DEVAstrom <antonsm18@gmail.com>
2026-09-25 11:26:10 +0200

    Revert "fixed walljumping jump sound and last crystal timer it was impossible with 10 sec"
    This reverts commit f2f7ab5eae57e99d2fe95c16b0e8d8bcf0573655.

--- Assets/Scripts/SoundHandler.cs
+++ Assets/Scripts/SoundHandler.cs
@@ -136,14 +136,12 @@ public class SoundHandler : MonoBehaviour
             {
                 StopMovementSfx();
                 PlayRandomSoundOfType(PlayerSoundType.Sprint, true);
-                
             }
             else
             {
                 AudioSource sprintAudio = GetValidAudioSource(PlayerSoundType.Sprint);
                 if (sprintAudio != null)
                     sprintAudio.Stop();
-                PlayRandomSoundOfType(PlayerSoundType.Jump, false);
             }
         }
         //updates grounded and continues playing the walk sound
